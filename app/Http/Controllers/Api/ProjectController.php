935632b60ad83a8b0b07e45a29ce59b82456948e
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-11-14 16:00:23 +0100

    Done Milestone#1

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+
+            $projects = Project::with("type","technologies")->get();
+            return response()->json(
+                [
+                    "success"=> true,
+                    "results"=> $projects,
+                
+                ]
+         );
+
+     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
+use App\Models\Project;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get("/projects",);
+Route::get("/projects", [ProjectController::class, "index"])->name("api.projects.index");
